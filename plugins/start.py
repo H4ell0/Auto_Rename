7974cd034d1fefb64224925e1b99aac7a2a23d17
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-14 23:35:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -33,17 +33,17 @@ async def start(client, message):
        )
     return
 
-@Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
-async def send_doc(client, message):
-    file = getattr(message, message.media.value)
-    filename = file.file_name
-    filesize = humanize.naturalsize(file.file_size)
-    fileid = file.file_id
-    await message.reply_text(
-        f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`""",
-        reply_to_message_id = message.id,
-        reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 𝚁𝙴𝙽𝙰𝙼𝙴 𝙽𝙾𝚆 📝",callback_data = "rename")],
-        [InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️",callback_data = "cancel")  ]]))
+#@Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
+#async def send_doc(client, message):
+   # file = getattr(message, message.media.value)
+#    filename = file.file_name
+ #   filesize = humanize.naturalsize(file.file_size)
+  #  fileid = file.file_id
+#    await message.reply_text(
+ #       f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`""",
+      #  reply_to_message_id = message.id,
+ #       reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 𝚁𝙴𝙽𝙰𝙼𝙴 𝙽𝙾𝚆 📝",callback_data = "rename")],
+        # [InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️",callback_data = "cancel")  ]]))
 
 
 @Client.on_callback_query()
