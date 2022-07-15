f0a71178fefc14da3831048bde92a12db71fae70
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-15 20:51:04 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -1,6 +1,6 @@
 from helper.utils import progress_for_pyrogram, convert
 from pyrogram import Client, filters
-from pyrogram.types import (  InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+from pyrogram.types import ( Message, InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from helper.database import find
