15a378758ab2539fd743b175b09dcea150470970
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-15 18:48:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import logging
 import logging.config
 from pyrogram import Client 
 
-logging.config.fileConfig("logging.conf")
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR) 
+#logging.config.fileConfig("logging.conf")
+#logging.getLogger().setLevel(logging.INFO)
+#logging.getLogger("pyrogram").setLevel(logging.ERROR) 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
@@ -13,7 +13,7 @@ API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
 
-FORCE_SUB = os.environ.get("FORCE_SUB", None)           
+#FORCE_SUB = os.environ.get("FORCE_SUB", None)           
 
 class Bot(Client):
 
@@ -33,20 +33,20 @@ class Bot(Client):
        me = await self.get_me()
        self.mention = me.mention
        self.username = me.username 
-       self.force_channel = FORCE_SUB
-       if FORCE_SUB:
-         try:
-            link = await self.export_chat_invite_link(FORCE_SUB)
-            self.invitelink = link
-         except Exception as e:
-            logging.warning(e) 
-            logging.warning("Make Sure Bot admin in force sub channel") 
-            self.force_channel = None
-       logging.info(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️ @{me.username}")
+       #self.force_channel = FORCE_SUB
+       #if FORCE_SUB:
+         #try:
+            #link = await self.export_chat_invite_link(FORCE_SUB)
+            #self.invitelink = link
+         #except Exception as e:
+            #logging.warning(e) 
+            #logging.warning("Make Sure Bot admin in force sub channel") 
+            #self.force_channel = None
+       #logging.info(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️ @{me.username}")
         
-    async def stop(self, *args):
-      await super().stop()
-      logging.info("Bot Stopped")
+    #async def stop(self, *args):
+      #await super().stop()
+      #logging.info("Bot Stopped")
         
 bot = Bot()
 bot.run()
